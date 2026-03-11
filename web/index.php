260377fae35264ec663276cf8981938c21d55330
Roman <babenk00r@yandex.ru>
2026-03-11 15:34:34 +0300

    fix: php-info

--- web/index.php
+++ web/index.php
@@ -1,11 +1,14 @@
 <?php
 
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
-
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../config/load-env.php';
+
+$yiiDebug = filter_var(getenv('YII_DEBUG') ?: '0', FILTER_VALIDATE_BOOLEAN);
+$yiiEnv = getenv('YII_ENV') ?: 'prod';
+
+defined('YII_DEBUG') or define('YII_DEBUG', $yiiDebug);
+defined('YII_ENV') or define('YII_ENV', $yiiEnv);
+
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/web.php';
